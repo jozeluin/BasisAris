ac9d9741863fc90e19aaa93b07d1d87e10bfd0b1
jozel <jozeluin@gmail.com>
2023-10-29 13:23:58 +0100

    Pc. Se crea dialogbox, el que surge al pulsar el floating button. Con el cual elegiremos el tipo de tarea y su nombre

--- app/src/main/java/com/example/aplicacionesvarias/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/aplicacionesvarias/todoapp/TodoActivity.kt
@@ -1,11 +1,13 @@
 package com.example.aplicacionesvarias.todoapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.aplicacionesvarias.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TodoActivity : AppCompatActivity() {
     private val categories=listOf(
@@ -28,17 +30,32 @@ class TodoActivity : AppCompatActivity() {
     private lateinit var rvTask:RecyclerView
     private lateinit var taskAdapter:TasksAdapter
 
+    private lateinit var fabAddTask:FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
         initComponent()
         initUI()
+        initListner()
+    }
+
+    private fun initListner() {
+        fabAddTask.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog(){
+        val dialog=Dialog(this)
+        dialog.setContentView(R.layout.dialog_task)
+        dialog.show()
     }
 
     private fun initComponent() {
         rvCategories=findViewById(R.id.rvCategories)
         rvTask=findViewById(R.id.rvTask)
+        fabAddTask=findViewById(R.id.fabAddTask)
 
     }
     private fun initUI() {
