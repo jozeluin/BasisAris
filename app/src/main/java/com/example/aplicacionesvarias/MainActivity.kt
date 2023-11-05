d741a315ae9bba9f1f051c228b593032079469d5
jozel <jozeluin@gmail.com>
2023-11-05 19:08:32 +0100

    Pc. 3:12 Proseguimos en el diseño xml. El eror de los margenes en el main ocurre al ulizar binding, si colocamos la programacion con el findviewbinding, todo va bien

--- app/src/main/java/com/example/aplicacionesvarias/MainActivity.kt
+++ app/src/main/java/com/example/aplicacionesvarias/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.aplicacionesvarias
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import androidx.core.view.marginTop
+import androidx.appcompat.app.AppCompatActivity
 import com.example.aplicacionesvarias.databinding.ActivityMainBinding
-import com.example.aplicacionesvarias.databinding.ActivitySettingsBinding
 import com.example.aplicacionesvarias.firstapp.ImcApp
 import com.example.aplicacionesvarias.settings.SettingsActivity
 import com.example.aplicacionesvarias.superheroapp.SuperHeroListActivity
@@ -15,48 +12,55 @@ import com.example.aplicacionesvarias.todoapp.TodoActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var bindig:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        bindig=ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bindig.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+       // setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+/*
+        val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+        val btnTODOApp = findViewById<Button>(R.id.btnTODO)
+        val btnSuperHero = findViewById<Button>(R.id.btnSuperHero)
+        val btnSettings = findViewById<Button>(R.id.btnSettings)
 
 
-
-
-        val btnSaludApp=findViewById<Button>(R.id.btnSaludApp)
-        val btnTODOApp=findViewById<Button>(R.id.btnTODO)
-        val btnSuperHero=findViewById<Button>(R.id.btnSuperHero)
-
-        btnSaludApp.setOnClickListener { navigateToSaludApp()  }
+        btnSaludApp.setOnClickListener { navigateToSaludApp() }
         btnTODOApp.setOnClickListener { navigateToTodoApp() }
-        btnSuperHero.setOnClickListener { navigateToSuperHeroApp ()}
-        bindig.btnSettings.setOnClickListener {  navigateToSettingsApp()}
+        btnSuperHero.setOnClickListener { navigateToSuperHeroApp() }
+        btnSettings.setOnClickListener { navigateToSettingsApp() }
+*/
+        initUI()
+    }
+    private fun initUI(){
+        binding.btnSaludApp.setOnClickListener { navigateToSaludApp() }
 
-
+        binding.btnTODO.setOnClickListener { navigateToTodoApp() }
+        binding.btnSuperHero.setOnClickListener { navigateToSuperHeroApp() }
+        binding.btnSettings.setOnClickListener { navigateToSettingsApp() }
     }
 
     private fun navigateToSettingsApp() {
-        val intent=Intent(this,SettingsActivity::class.java)
+        val intent = Intent(this, SettingsActivity::class.java)
         startActivity(intent)
     }
 
     private fun navigateToSuperHeroApp() {
-        val intent=Intent(this,SuperHeroListActivity::class.java)
+        val intent = Intent(this, SuperHeroListActivity::class.java)
         startActivity(intent)
 
     }
 
     private fun navigateToTodoApp() {
-        bindig
-        val intent=Intent(this,TodoActivity::class.java)
+        //bindig
+        val intent = Intent(this, TodoActivity::class.java)
         startActivity(intent)
     }
 
-    private fun navigateToSaludApp(){
-        val intent=Intent(this,ImcApp::class.java)
+    private fun navigateToSaludApp() {
+        val intent = Intent(this, ImcApp::class.java)
         startActivity(intent)
     }
 }
